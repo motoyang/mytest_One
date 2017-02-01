069cd92f25e5ec4b3ba7868cb0e9cca7d9f1fa73
moto2 yang <moto2yang@UltrabookPro-c16b.local>
2017-02-01 15:45:59 +0800

    first

---
+++ One/One/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  One
+//
+//  Created by moto2 yang on 2017/2/1.
+//  Copyright © 2017年 moto2 yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
